e6ec4a2a954e57ab45671f2d9e2fd65e28b6cddb
mrsakurada-star <mr.sakurada@gmail.com>
2026-06-18 12:13:34 +0900

    auto: _print_check.js

--- Site Work Order/_print_check.js
+++ Site Work Order/_print_check.js
@@ -43,7 +43,7 @@ const path = require('path');
   });
   console.log('print doc size px:', printBox);
 
-  await page.pdf({ path: '_print_preview.pdf', format: 'A4', landscape: true, printBackground: true, margin: { top: '8mm', bottom: '8mm', left: '8mm', right: '8mm' } });
+  await page.pdf({ path: 'C:/Temp_pwcheck_out.pdf', format: 'A4', landscape: true, printBackground: true, margin: { top: '8mm', bottom: '8mm', left: '8mm', right: '8mm' } });
   console.log('pdf saved');
 
   await browser.close();
